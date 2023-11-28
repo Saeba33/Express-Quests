08592a4c6301c9b7054e5abc3b55c80e1ff6411e
Charles Proust <konoha33@hotmail.fr>
2023-11-28 11:54:51 +0100

    express 02 finished

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
+app.post("/api/movies", movieControllers.postMovie);
+
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
 app.get("/api/users", userControllers.getUsers);
+app.post("/api/users", userControllers.postUser);
+
 app.get("/api/users/:id", userControllers.getUserById);
 
 module.exports = app;
